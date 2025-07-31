4cc51505cd2bf0b3cd35350f221f016141f74ee2
oe-cubas <oe.cubas@gmail.com>
2025-07-31 11:42:15 -0600

    Modificacion de texto en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          HOLA MUNDO DESDE REACT + GITHUB
         </p>
       </div>
       <p className="read-the-docs">
